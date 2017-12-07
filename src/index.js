dadfe20b143e16d05717b92e1df1dcff9354aa4e
sangwoo89118 <sangwoo89118@gmail.com>
2017-12-06 18:23:35 -0800

    Feature set 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const luckyNumber = () => Math.floor((Math.random()*1000)+1);
+
+function greeting(person){
+    return <div className="container">
+        <h1>Hello World {person.name}</h1>
+        <h2 className="text-muted">Your lucky number is: {person.luckyNumber}</h2>
+    </div>
+}
+
+const user = {
+    name: "Sangwoo",
+    luckyNumber: luckyNumber()
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user) ,
     document.getElementById('root')
 );
+
