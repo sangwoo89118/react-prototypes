9b1fef0cd359fb2ca1717921a74022b59ac06638
sangwoo89118 <sangwoo89118@gmail.com>
2017-12-08 11:24:22 -0800

    stopped working on this branch because of the lecture

---
+++ src/components/table.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default () =>{
+    return (
+        <table className="table">
+            <thead className="thead-inverse">
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Grade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Sangwoo</th>
+                    <th>React</th>
+                    <th>100</th>
+                </tr>
+                <tr>
+                    <th>Chung</th>
+                    <th>PHP</th>
+                    <th>100</th>
+                </tr>
+                <tr>
+                    <th>Christine</th>
+                    <th>jQuery</th>
+                    <th>100</th>
+                </tr>
+            </tbody>
+        </table>
+
+
+    )
+}
